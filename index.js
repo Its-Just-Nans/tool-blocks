c9d749c11fc3749ed420f3e6b3598ee2d8726055
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-03-20 23:00:43 +0100

    [fix] first commit

---
+++ index.js
@@ -0,0 +1,1 @@
+window.api.send("toMain", "codage-ligne.html");
---
+++ main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+
+let win2;
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            nodeIntegration: false,
+            contextIsolation: true,
+        },
+    });
+    win.loadFile("index.html");
+    win2 = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            nodeIntegration: false,
+            contextIsolation: true,
+        },
+    });
+    win2.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+ipcMain.on("toMain", (event, args) => {
+    win.loadFile(args);
+});
